e528cf812c6b34168d99808c0e72a2cb873759dc
Peter Cusack & rickypchen <pair+petercusack.rickypchen@devbootcamp.com>
2015-03-16 16:05:27 -0700

    add questions controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'questions/index'
+
   resources :questions, only: [:index, :show]
-  root 'welcome#index'
+  root 'questions#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

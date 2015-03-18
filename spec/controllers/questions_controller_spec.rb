22c7ada51b79404883ffb6df5d6c45bd755f6c23
Peter Cusack & rickypchen <pair+petercusack.rickypchen@devbootcamp.com>
2015-03-17 21:12:16 -0700

    added test for update route

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -30,17 +30,39 @@ end
 
 describe "DELETE #destroy" do
   before (:each) do
-    question = FactoryGirl.create(:question)
-    delete :destroy, {id: question.id}
+    @question = FactoryGirl.create(:question)
+  end
+
+  it "should have a question in the database " do
+      expect(Question.all.count).to eq(1)
   end
 
   it "deletes a question" do
+    delete :destroy, {id: @question.id}
     expect(Question.all.count).to eq(0)
   end
 
   it "redirects to index page" do
+    delete :destroy, {id: @question.id}
     expect(response).to be_redirect
   end
+end
+
+describe "PUT #update" do
+  before (:each) do
+    @question = FactoryGirl.create(:question)
+  end
+
+  it "redirects if edits are valid" do
+    put :update, id: @question.id, question: {title: @question.title, content: @question.content}
+    expect(response).to be_redirect
+  end
+
+  it "does not redirects if edits are valid" do
+    put :update, id: @question.id, question: {title: @question.title, content: nil}
+    expect(response).to_not be_redirect
+  end
+
 end
 
 
